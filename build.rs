ad2d2dbc06cb215015900a447e460e4ea7d893c8
Andrew Straw <strawman@astraw.com>
2016-12-29 21:04:35 +0100

    add support for env-var based config

--- build.rs
+++ build.rs
@@ -1,5 +1,37 @@
 extern crate pkg_config;
 
+use std::env;
+
 fn main() {
-    pkg_config::Config::new().atleast_version("2.2.4").probe("libdc1394-2").unwrap();
+
+    enum LocationSource {
+        EnvVar(String),
+        PkgConfig,
+    }
+
+    let loc_source = match env::var("DC1394_LIBDIR") {
+        Ok(dir) => LocationSource::EnvVar(dir),
+        Err(_) => LocationSource::PkgConfig,
+    };
+
+    match loc_source {
+        LocationSource::EnvVar(dir) => {
+            println!("cargo:rustc-link-search=native={}",dir);
+            println!("cargo:rustc-link-lib=static=dc1394");
+        },
+        LocationSource::PkgConfig => {
+            let result = pkg_config::Config::new()
+                .atleast_version("2.1.2")
+                .statik(true)
+                .probe("libdc1394-2");
+            match result {
+                Ok(_) => {},
+                Err(e) => {
+                    panic!("No env var DC1394_LIBDIR could be read and pkg_config \
+                           failed with {}", e);
+                }
+            }
+        }
+    }
+
 }
